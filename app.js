3b0671832da3b42af3d96c9a7e7ccb0cafd9ec05
ElPassio <passiotti.agustin@gmail.com>
2024-06-22 16:29:44 -0300

    pagina subida

---
+++ app.js
@@ -0,0 +1,104 @@
+let url = "https://api-operaciones.onrender.com";
+
+document.getElementById("suma").addEventListener("click", () => {
+    n1 = document.getElementById("numero1").value;
+    n2 = document.getElementById("numero2").value;
+    url = `https://api-operaciones.onrender.com/api/suma?num1=${n1}&num2=${n2}`;
+    consumirAPI(url);
+});
+
+document.getElementById("resta").addEventListener("click", () => {
+  n1 = document.getElementById("numero1").value;
+  n2 = document.getElementById("numero2").value;
+  url = `https://api-operaciones.onrender.com/api/resta?num1=${n1}&num2=${n2}`;
+  consumirAPI(url);
+});
+
+document.getElementById("multi").addEventListener("click", () => {
+  n1 = document.getElementById("numero1").value;
+  n2 = document.getElementById("numero2").value;
+  url = `https://api-operaciones.onrender.com/api/multiplicacion?num1=${n1}&num2=${n2}`;
+  consumirAPI(url);
+});
+
+document.getElementById("div").addEventListener("click", () => {
+  n1 = document.getElementById("numero1").value;
+  n2 = document.getElementById("numero2").value;
+  url = `https://api-operaciones.onrender.com/api/division?num1=${n1}&num2=${n2}`;
+  consumirAPI(url);
+});
+
+document.getElementById("pow").addEventListener("click", () => {
+  n1 = document.getElementById("numero1").value;
+  n2 = document.getElementById("numero2").value;
+  url = `https://api-operaciones.onrender.com/api/potencia?num1=${n1}&num2=${n2}`;
+  consumirAPI(url);
+});
+
+function consumirAPI(url) {
+  // Realizar la solicitud GET a la API utilizando fetch
+  fetch(url)
+    .then((response) => {
+      // Verificar si la respuesta es exitosa (código de estado HTTP 200-299)
+      if (!response.ok) {
+        throw new Error("Ocurrió un error al obtener los datos");
+      }
+      // Convertir la respuesta a formato JSON
+      console.log("respuesta json");
+      return response.json();
+    })
+    .then((data) => {
+      // trabajar con los datos obtenidos de la API
+      console.log("Datos recibidos:", data);
+      document.getElementById("resultado").innerText = "Resultado: " + data.resultado;
+      // Puedes retornar los datos o realizar otras operaciones aquí
+    })
+    .catch((error) => {
+      // Capturar errores de red o de la API
+      console.error("Error en la solicitud:", error);
+    });
+}
+
+function mostrarOp(op) {
+  const letraHover = document.getElementById('op');
+  letraHover.textContent = op; // Establecer la letra
+
+  // Opcional: Ajustar estilos para centrar la letra
+  letraHover.style.display = 'inline-block';
+  letraHover.style.position = 'absolute';
+  letraHover.style.left = '50%';
+  letraHover.style.transform = 'translateX(-50%)';
+}
+
+// Opcional: Restaurar el contenido al quitar el hover del botón
+function limpiarOp() {
+  const letraHover = document.getElementById('op');
+  letraHover.textContent = '?'; // Limpiar el contenido de la letra
+}
+
+function cambiarOperador() {
+const botones = document.querySelectorAll('.caja-button button');
+
+// Iterar sobre cada botón y agregar un event listener para el evento 'mouseover'
+botones.forEach(boton => {
+    boton.addEventListener('mouseout', limpiarOp);
+});
+}
+
+document.getElementById("suma").addEventListener('mouseover', () => {
+  document.getElementById("op").innerText = "+";
+});
+document.getElementById("resta").addEventListener('mouseover', () => {
+  document.getElementById("op").innerText = "-";
+});
+document.getElementById("multi").addEventListener('mouseover', () => {
+  document.getElementById("op").innerText = "x";
+});
+document.getElementById("div").addEventListener('mouseover', () => {
+  document.getElementById("op").innerText = "/";
+});
+document.getElementById("pow").addEventListener('mouseover', () => {
+  document.getElementById("op").innerText = "^";
+});
+
+document.addEventListener('DOMContentLoaded', cambiarOperador);
